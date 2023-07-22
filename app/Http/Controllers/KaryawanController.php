817144a85674131fc966156da517997038917808
nbagass <narukamibagaskara@gmail.com>
2023-07-22 11:47:14 +0700

    Create view cuti + set trigger

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Karyawan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Resources\PostDetailResource;
 
 class KaryawanController extends Controller
